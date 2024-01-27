2cd3486dc7205bdf605fcabb01b418fd8abcbf9f
hwchua0209 <hwchua0209@gmail.com>
2024-01-27 08:49:17 +0000

    update for gcp vm

--- 01-docker-terraform/terraform/variables.tf
+++ 01-docker-terraform/terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "./keys/my_creds.json"
+  default     = "~/.gc/my_creds.json"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
